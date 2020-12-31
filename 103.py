174168485267d186755a6eab42bdbf4ebfcc29bd
DeokwonWang <wangpr95@gmail.com>
2020-12-31 13:20:22 +0900

    commit -m

---
+++ 103.py
@@ -0,0 +1,19 @@
+solarsys = ['태양','수성','금성','지구','화성','목성','토성','천왕성','해왕성']
+rock_planet = solarsys[1:4]
+gas_planet = solarsys[4:]
+print('태양계의 암석형 행성: ',end='');print(rock_planet)
+print('태양계의 가스형 행성: ',end='');print(gas_planet)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 104.py
@@ -0,0 +1,22 @@
+listdata = list(range(1,21))
+evenlist = listdata[1::2]
+print(evenlist)
+
+oddlist = listdata[::2]
+print(oddlist)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
